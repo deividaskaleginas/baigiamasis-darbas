52b4c187504ed2dbdefdfd285db000098d2082af
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-02-10 15:28:19 +0200

    Deleted not useful files

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { ForumUserProvider } from "./context/forumUserContext";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ForumUserProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ForumUserProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
